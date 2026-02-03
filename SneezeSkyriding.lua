063742ddc479094628d941034713a523b828b8c0
JD <72483040+DancyNerd@users.noreply.github.com>
2026-02-03 06:17:35 -0500

    Fixed Event Frame
    Event frame now has SetScript logic, so it actually fires.

--- SneezeSkyriding.lua
+++ SneezeSkyriding.lua
@@ -13,41 +13,48 @@ local regulatingEvents = CreateFrame("FRAME")
 local continueRunning = false
 
 
-local TIME_INTERVAL = 0.5
+local TIME_INTERVAL = 0.25
 
 
 
 
+local eventHandlers = {
+    ["PLAYER_IS_GLIDING_CHANGED"] = function(self, event, ...)
+        local playerIsGliding = ...
+        if not playerIsGliding then
+            continueRunning = false
+        end
+    end
+}
 
+for eventName in pairs(eventHandlers) do
+    regulatingEvents:RegisterEvent(eventName)
+end
+
+regulatingEvents:SetScript("OnEvent", function(self, event, ...)
+	local handler = eventHandlers[event]
+	if not handler then
+		return
+	end
+--	print("handler found for event: "..event)
+	handler(self, event, ...)
+end)
 
 local function runTimer()
-    if not continueRunning then
-        print("stopped")
+    if not continueRunning or elapsed>68 then
+        print("stopped at " ..elapsed)
         elapsed = 0
+        continueRunning = false
     elseif continueRunning then
         elapsed = elapsed + TIME_INTERVAL
         C_Timer.After(TIME_INTERVAL, function() print(elapsed) runTimer() end)
-
     end
 end
 
 
 function private.startTimer()
-    print("we made it")
+    --print("we made it")
     continueRunning = true
     runTimer()
 end
 
-local eventTable = {
-    ["PLAYER_IS_GLIDING_CHANGED"] = function(self, event, ...)
-        local playerIsGliding = ...
-        if not playerIsGliding then
-            continueRunning = false
-            runTimer()
-        end
-    end
-}
-
-for eventName in pairs(eventTable) do
-    regulatingEvents:RegisterEvent(eventName)
-end
