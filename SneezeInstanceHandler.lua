b14d1268c7f969c41dd1772544140007bd08faf0
JD <72483040+DancyNerd@users.noreply.github.com>
2026-02-05 16:38:10 -0500

    Instance Mapping Skeleton Setup
    Setup skeleton logic to begin breaking instance types out to be handled appropriately.

--- SneezeInstanceHandler.lua
+++ SneezeInstanceHandler.lua
@@ -6,15 +6,21 @@ Specified eveents from Sneeze.lua will be used to trigger functions here.
 local addonName, private = ...
 print("Yes, this is empty right now.")
 
+private.instanceTypeMap = {
+    
+}
 
 
 
 
+function private.instanceType(typeOfInstance)
+    
+end
 
-local function findStory(isDelve)
+local function findStory(delveID)
     --Find the current story arc for the current delve.
 end
 
-function private.inDelve()
+local function inDelve()
     --We need to know which delve the player is in.
 end
